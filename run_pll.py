744b312f9ef3ccd2ea7fec45f2e150ae297ea1b6
weiyumou <weiy0007@e.ntu.edu.sg>
2019-11-27 10:52:42 -0500

    Added apex and distributed training

--- run_pll.py
+++ run_pll.py
@@ -1,4 +1,5 @@
 import argparse
+import logging
 import os
 import random
 import time
@@ -6,10 +7,13 @@ import time
 import numpy as np
 import torch
 from torch.utils.data import DataLoader
+from torch.utils.data import RandomSampler, DistributedSampler
 from transformers import BertConfig, BertForSequenceClassification, BertTokenizer
 
 import train
 from data import WikiReader, WikiTrainDataset
+
+logger = logging.getLogger(__name__)
 
 
 def parse_args():
@@ -21,10 +25,6 @@ def parse_args():
                         type=str,
                         help='Path to the data file',
                         required=True)
-    parser.add_argument('--model_dir',
-                        type=str,
-                        help='Path to save models',
-                        default="models")
     parser.add_argument('--num_lines',
                         type=int,
                         help='Number of lines to read',
@@ -41,9 +41,6 @@ def parse_args():
                         type=int,
                         help='Maximum number of segments',
                         default=64)
-    parser.add_argument('--do_lower',
-                        help='Whether to use lower case letters',
-                        action="store_true")
     parser.add_argument('--num_epochs',
                         type=int,
                         help='Number of epochs',
@@ -62,48 +59,104 @@ def parse_args():
                         default=2)
     parser.add_argument('--learn_prd',
                         type=int,
-                        help='Number of epochs before providing harder examples',
-                        default=10)
+                        help='Number of steps before providing harder examples',
+                        default=1000)
     parser.add_argument('--eval',
                         help='Whether to evaluate the model',
                         action="store_true")
     parser.add_argument('--resume',
                         type=str,
                         help='Path to a saved checkpoint',
-                        default="")
+                        default=None)
+    parser.add_argument("--per_gpu_train_batch_size", default=8, type=int,
+                        help="Batch size per GPU/CPU for training.")
+    parser.add_argument('--gradient_accumulation_steps', type=int, default=1,
+                        help="Number of updates steps to accumulate before performing a backward/update pass.")
+    parser.add_argument("--max_grad_norm", default=1.0, type=float,
+                        help="Max gradient norm.")
+    parser.add_argument("--max_steps", default=-1, type=int,
+                        help="If > 0: set total number of training steps to perform. Override num_train_epochs.")
+    parser.add_argument('--logging_steps', type=int, default=50,
+                        help="Log every X updates steps.")
+    parser.add_argument('--save_steps', type=int, default=50,
+                        help="Save checkpoint every X updates steps.")
+    parser.add_argument('--fp16', action='store_true',
+                        help="Whether to use 16-bit (mixed) precision (through NVIDIA apex) instead of 32-bit")
+    parser.add_argument('--fp16_opt_level', type=str, default='O1',
+                        help="For fp16: Apex AMP optimization level selected in ['O0', 'O1', 'O2', and 'O3']."
+                             "See details at https://nvidia.github.io/apex/amp.html")
+    parser.add_argument("--local_rank", type=int, default=-1,
+                        help="For distributed training: local_rank")
     args = parser.parse_args()
     return args
 
 
 def main():
     args = parse_args()
+    resume = args.resume
+    if resume is not None:
+        args = torch.load(os.path.join(resume, "training_args.bin"))
+        args.resume = resume
+
+    # Setup CUDA, GPU & distributed training
+    if args.local_rank == -1:
+        device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+        args.n_gpu = torch.cuda.device_count()
+    else:  # Initializes the distributed backend which will take care of sychronizing nodes/GPUs
+        torch.cuda.set_device(args.local_rank)
+        device = torch.device("cuda", args.local_rank)
+        torch.distributed.init_process_group(backend='nccl')
+        args.n_gpu = 1
+    args.device = device
+
     if args.deterministic:
         random.seed(0)
         torch.manual_seed(0)
         np.random.seed(0)
         torch.backends.cudnn.deterministic = True
+        if args.n_gpu > 0:
+            torch.cuda.manual_seed_all(0)
 
-    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-    model_dir = os.path.join(args.model_dir, time.ctime().replace(" ", "_").replace(":", "_"))
-    os.makedirs(model_dir, exist_ok=True)
+    # Setup logging
+    logging.basicConfig(format='%(asctime)s - %(levelname)s - %(name)s -   %(message)s',
+                        datefmt='%m/%d/%Y %H:%M:%S',
+                        level=logging.INFO if args.local_rank in [-1, 0] else logging.WARN)
+    logger.warning("Process rank: %s, device: %s, n_gpu: %s, distributed training: %s, 16-bits training: %s",
+                   args.local_rank, device, args.n_gpu, bool(args.local_rank != -1), args.fp16)
+
+    if args.local_rank not in [-1, 0]:
+        torch.distributed.barrier()  # Make sure only the first process in distributed training will download model & vocab
+
+    args.output_dir = os.path.join("models", time.ctime().replace(" ", "_").replace(":", "_"))
+    os.makedirs(args.output_dir, exist_ok=True)
 
     tokeniser = BertTokenizer.from_pretrained("bert-base-cased")
-    config_json = {
-        "vocab_size": tokeniser.vocab_size,
-        "hidden_size": 192,  # 768
-        "num_hidden_layers": 3,  # 12
-        "num_attention_heads": 3,  # 12
-        "intermediate_size": 768,  # 3072
-        "hidden_act": "gelu",
-        "hidden_dropout_prob": 0.1,
-        "attention_probs_dropout_prob": 0.1,
-        "max_position_embeddings": args.max_seq_len,
-        "type_vocab_size": args.max_num_segs,
-        "initializer_range": 0.02,
-        "layer_norm_eps": 1e-12,
-        "num_labels": args.max_num_segs ** 2
-    }
-    bert_config = BertConfig(**config_json)
+    if args.resume is not None:
+        model = BertForSequenceClassification.from_pretrained(args.resume)
+    else:
+        config_json = {
+            "vocab_size": tokeniser.vocab_size,
+            "hidden_size": 192,  # 768
+            "num_hidden_layers": 3,  # 12
+            "num_attention_heads": 3,  # 12
+            "intermediate_size": 768,  # 3072
+            "hidden_act": "gelu",
+            "hidden_dropout_prob": 0.1,
+            "attention_probs_dropout_prob": 0.1,
+            "max_position_embeddings": args.max_seq_len,
+            "type_vocab_size": args.max_num_segs,
+            "initializer_range": 0.02,
+            "layer_norm_eps": 1e-12,
+            "num_labels": args.max_num_segs ** 2
+        }
+        bert_config = BertConfig(**config_json)
+        model = BertForSequenceClassification(bert_config)
+
+    if args.local_rank == 0:
+        torch.distributed.barrier()  # Make sure only the first process in distributed training will download model & vocab
+
+    model.to(args.device)
+    logger.info("Training/evaluation parameters %s", args)
 
     wiki_reader = WikiReader(args.data_file, args.num_lines)
     # wiki_reader.split(train_perct=0.9)
@@ -113,15 +166,15 @@ def main():
                                      args.num_segs, args.max_num_segs)
     # val_dataset = WikiEvalDataset(wiki_reader.val_set, tokeniser, args.max_seq_len, args.max_num_segs)
 
+    args.train_batch_size = args.per_gpu_train_batch_size * max(1, args.n_gpu)
+    train_sampler = RandomSampler(train_dataset) if args.local_rank == -1 else DistributedSampler(train_dataset)
     dataloaders = {
-        "train": DataLoader(train_dataset, batch_size=args.train_batch_size, shuffle=True),
+        "train": DataLoader(train_dataset, sampler=train_sampler, batch_size=args.train_batch_size),
         # "val": DataLoader(val_dataset, batch_size=args.eval_batch_size, shuffle=False)
     }
 
-    model = BertForSequenceClassification(bert_config)
-    train.train_model(device, model, dataloaders,
-                      tokeniser, args.num_epochs,
-                      args.num_segs, args.max_num_segs, model_dir)
+    global_step, tr_loss = train.train_model(device, model, dataloaders, args, logger)
+    logger.info(" global_step = %s, average loss = %s", global_step, tr_loss)
 
 
 if __name__ == '__main__':
--- train.py
+++ train.py
@@ -1,16 +1,10 @@
-import logging
 import os
 
 import torch
 import tqdm
-from transformers import WEIGHTS_NAME, CONFIG_NAME
+from torch.utils.tensorboard import SummaryWriter
 
 from RAdam.radam import RAdam
-
-logging.basicConfig(format='%(asctime)s - %(levelname)s - %(name)s -   %(message)s',
-                    datefmt='%m/%d/%Y %H:%M:%S',
-                    level=logging.INFO)
-logger = logging.getLogger(__name__)
 
 
 def calc_ranks(x):
@@ -20,59 +14,138 @@ def calc_ranks(x):
     return preds
 
 
-def train_model(device, model, dataloaders, tokeniser, num_epochs, num_segs, max_num_segs, model_dir):
-    model = model.to(device)
+def train_model(device, model, dataloaders, args, logger):
+    if args.local_rank in [-1, 0]:
+        tb_writer = SummaryWriter()
+
+    if args.max_steps > 0:
+        t_total = args.max_steps
+        args.num_epochs = args.max_steps // (len(dataloaders["train"]) // args.gradient_accumulation_steps) + 1
+    else:
+        t_total = len(dataloaders["train"]) // args.gradient_accumulation_steps * args.num_epochs
+
     optimiser = RAdam(model.parameters())
     criterion = torch.nn.CrossEntropyLoss()
 
-    curr_num_epochs = 0
-    for epoch in range(num_epochs):
-        running_loss, loss_total = 0, 0
-        running_corrects, acc_total = 0, 0
+    # Apex
+    if args.fp16:
+        try:
+            from apex import amp
+        except ImportError:
+            raise ImportError("Please install apex from https://www.github.com/nvidia/apex to use fp16 training.")
+        model, optimiser = amp.initialize(model, optimiser, opt_level=args.fp16_opt_level)
+
+    # multi-gpu training (should be after apex fp16 initialization)
+    if args.n_gpu > 1:
+        model = torch.nn.DataParallel(model)
+
+    # Distributed training (should be after apex fp16 initialization)
+    if args.local_rank != -1:
+        model = torch.nn.parallel.DistributedDataParallel(model, device_ids=[args.local_rank],
+                                                          output_device=args.local_rank,
+                                                          find_unused_parameters=True)
+
+    logger.info("***** Running training *****")
+    logger.info("  Num examples = %d", len(dataloaders["train"].dataset))
+    logger.info("  Num Epochs = %d", args.num_epochs)
+    logger.info("  Instantaneous batch size per GPU = %d", args.per_gpu_train_batch_size)
+    logger.info("  Total train batch size (w. parallel, distributed & accumulation) = %d",
+                args.train_batch_size * args.gradient_accumulation_steps * (
+                    torch.distributed.get_world_size() if args.local_rank != -1 else 1))
+    logger.info("  Gradient Accumulation steps = %d", args.gradient_accumulation_steps)
+    logger.info("  Total optimization steps = %d", t_total)
+
+    curr_num_steps, global_step = 0, 0
+    num_segs = args.num_segs
+    tr_loss, logging_loss = 0.0, 0.0
+    model.zero_grad()
+    if args.resume is not None:
+        checkpoint = torch.load(os.path.join(args.resume, "states.bin"))
+        optimiser.load_state_dict(checkpoint["optimiser_state_dict"])
+        curr_num_steps, global_step = checkpoint["curr_num_steps"], checkpoint["global_step"]
+        num_segs = checkpoint["num_segs"]
+    train_iterator = tqdm.trange(args.num_epochs, desc="Epoch", disable=args.local_rank not in [-1, 0])
+    for _ in train_iterator:
         model.train()
-        for input_ids, seg_ids, att_masks, perms in tqdm.tqdm(dataloaders["train"], desc="Training"):
+        epoch_iterator = tqdm.tqdm(dataloaders["train"], desc="Iteration", disable=args.local_rank not in [-1, 0])
+        running_corrects, acc_total = 0, 0
+        for step, batch in enumerate(epoch_iterator):
+            input_ids, seg_ids, att_masks, perms = batch
             input_ids = input_ids.to(device)
             seg_ids = seg_ids.to(device)
             att_masks = att_masks.to(device)
             labels = torch.flatten(perms).to(device)
 
             outputs, *_ = model(input_ids=input_ids, attention_mask=att_masks, token_type_ids=seg_ids)
-            outputs = outputs.reshape(-1, max_num_segs, max_num_segs)[:, :num_segs, :num_segs].reshape(-1, num_segs)
+            outputs = outputs.reshape(-1, args.max_num_segs, args.max_num_segs)[:, :num_segs, :num_segs].reshape(-1,
+                                                                                                                 num_segs)
 
             loss = criterion(outputs, labels)
-            optimiser.zero_grad()
-            loss.backward()
-            optimiser.step()
+            if args.n_gpu > 1:
+                loss = loss.mean()  # mean() to average on multi-gpu parallel training
+            if args.gradient_accumulation_steps > 1:
+                loss /= args.gradient_accumulation_steps
 
-            preds = torch.argmax(outputs, dim=-1)
-            running_corrects += torch.sum(preds == labels).item()
-            acc_total += labels.numel()
-            running_loss += loss.item() * labels.numel()
-            loss_total += labels.numel()
+            if args.fp16:
+                with amp.scale_loss(loss, optimiser) as scaled_loss:
+                    scaled_loss.backward()
+            else:
+                loss.backward()
+            tr_loss += loss.item()
 
-        train_loss, train_acc = running_loss / loss_total, running_corrects / acc_total
-        logger.info(f"Epoch {epoch}: # Segs = {num_segs}, Train Loss = {train_loss}, Train Acc = {train_acc}")
+            if args.local_rank in [-1, 0]:
+                preds = torch.argmax(outputs, dim=-1)
+                running_corrects += torch.sum(preds == labels).item()
+                acc_total += labels.numel()
 
-        # eval_loss, eval_acc = eval_model(device, model, dataloaders["val"], num_segs, max_num_segs)
-        # logger.info(f"Epoch {epoch}: Val Loss = {eval_loss}, Val Acc = {eval_acc}")
+            if (step + 1) % args.gradient_accumulation_steps == 0:
+                if args.fp16:
+                    torch.nn.utils.clip_grad_norm_(amp.master_params(optimiser), args.max_grad_norm)
+                else:
+                    torch.nn.utils.clip_grad_norm_(model.parameters(), args.max_grad_norm)
 
-        output_dir = os.path.join(model_dir, f"epoch_{epoch}-num_segs_{num_segs}")
-        os.makedirs(output_dir, exist_ok=True)
-        model_to_save = model.module if hasattr(model, 'module') else model
+                optimiser.step()
+                model.zero_grad()
+                global_step += 1
+                curr_num_steps += 1
 
-        output_model_file = os.path.join(output_dir, WEIGHTS_NAME)
-        output_config_file = os.path.join(output_dir, CONFIG_NAME)
+                if args.local_rank in [-1, 0] and args.logging_steps > 0 and global_step % args.logging_steps == 0:
+                    train_acc = running_corrects / acc_total
+                    loss = (tr_loss - logging_loss) / args.logging_steps
+                    tb_writer.add_scalar("train_acc", train_acc, global_step)
+                    tb_writer.add_scalar("loss", loss, global_step)
+                    logging_loss = tr_loss
+                    logger.info(f"\nStep {global_step}: Train Acc = {train_acc}, Train Loss = {loss}")
 
-        torch.save(model_to_save.state_dict(), output_model_file)
-        model_to_save.config.to_json_file(output_config_file)
-        tokeniser.save_vocabulary(output_dir)
+                if args.local_rank in [-1, 0] and curr_num_steps == num_segs * args.learn_prd and num_segs < args.max_num_segs:
+                    curr_num_steps = 0
+                    num_segs += 1
+                    dataloaders["train"].dataset.set_num_segs(num_segs)
+                    dataloaders["train"].dataset.set_poisson_rate(num_segs)
 
-        curr_num_epochs += 1
-        if curr_num_epochs == num_segs and num_segs < max_num_segs:
-            curr_num_epochs = 0
-            num_segs += 1
-            dataloaders["train"].dataset.set_num_segs(num_segs)
-            dataloaders["train"].dataset.set_poisson_rate(num_segs)
+                if args.local_rank in [-1, 0] and args.save_steps > 0 and global_step % args.save_steps == 0:
+                    # Save model checkpoint
+                    output_dir = os.path.join(args.output_dir, f"checkpoint-{global_step}")
+                    os.makedirs(output_dir, exist_ok=True)
+                    model_to_save = model.module if hasattr(model, "module") else model
+                    model_to_save.save_pretrained(output_dir)
+                    torch.save(args, os.path.join(output_dir, "training_args.bin"))
+                    torch.save(
+                        {"optimiser_state_dict": optimiser.state_dict(),
+                         "global_step": global_step,
+                         "num_segs": num_segs,
+                         "curr_num_steps": curr_num_steps}, os.path.join(output_dir, "states.bin"))
+                    logger.info(f"Saving model checkpoint to {output_dir}")
+
+            if 0 < args.max_steps < global_step:
+                epoch_iterator.close()
+                break
+        if 0 < args.max_steps < global_step:
+            train_iterator.close()
+            break
+    if args.local_rank in [-1, 0]:
+        tb_writer.close()
+    return global_step, tr_loss / global_step
 
 
 def eval_model(device, model, dataloader, num_segs, max_num_segs):
