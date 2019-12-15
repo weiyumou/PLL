Fixed mean calculation

data.py
@@ -136,7 +136,7 @@ class WikiDataset(Dataset):
 
         token_ids = torch.tensor(token_ids)
         seg_ids = torch.tensor(seg_ids)
-        mask = torch.ones_like(token_ids, dtype=torch.long)
+        mask = torch.ones_like(token_ids, dtype=torch.bool)
 
         token_ids = torch.nn.functional.pad(token_ids, [0, self.max_seq_len - token_ids.size(0)])
         seg_ids = torch.nn.functional.pad(seg_ids, [0, self.max_seq_len - seg_ids.size(0)])
@@ -162,22 +162,12 @@ class WikiTrainDataset(WikiDataset):
             doc.append(self._prep_inputs(sent_segs, perm))
         token_ids, token_seg_ids, token_masks = [torch.stack(item) for item in zip(*doc)]
 
-        # sent_segs_id = torch.arange(self.sents_per_doc, dtype=torch.long)
-        # sent_mask = torch.ones(self.sents_per_doc, dtype=torch.long)
         k = self.sent_pdist.sample().int().item()
         sent_perm = torch.tensor(k_permute(self.sents_per_doc, k, self.sent_ds))
 
         token_ids = token_ids[sent_perm]
         token_seg_ids = token_seg_ids[sent_perm]
         token_masks = token_masks[sent_perm]
-
-        # num_pads = self.sents_per_doc - num_sents
-        # token_ids = torch.nn.functional.pad(token_ids, [0, 0, 0, num_pads])
-        # token_seg_ids = torch.nn.functional.pad(token_seg_ids, [0, 0, 0, num_pads])
-        # token_masks = torch.nn.functional.pad(token_masks, [0, 0, 0, num_pads])
-        # sent_segs_id = torch.nn.functional.pad(sent_segs_id, [0, num_pads])
-        # sent_mask = torch.nn.functional.pad(sent_mask, [0, num_pads])
-        # sent_perm = torch.nn.functional.pad(sent_perm, [0, num_pads])
 
         return token_ids, token_seg_ids, token_masks, sent_perm
 
@@ -197,9 +187,6 @@ class WikiEvalDataset(WikiDataset):
     def __getitem__(self, index: int):
         doc = self.docs[index]
         token_ids, token_seg_ids, token_masks = [torch.stack(item) for item in zip(*doc)]
-
-        # sent_segs_id = torch.arange(self.sents_per_doc, dtype=torch.long)
-        # sent_mask = torch.ones(self.sents_per_doc, dtype=torch.long)
         sent_perm = self.sent_perms[index]
 
         token_ids = token_ids[sent_perm]

models.py
@@ -13,7 +13,8 @@ class HiBERT(BertPreTrainedModel):
 
     def forward(self, token_ids, token_seg_ids, token_masks, n, s):
         sent_enc_out, *_ = self.sent_enc(input_ids=token_ids, attention_mask=token_masks, token_type_ids=token_seg_ids)
-        sent_enc_out = torch.mean(sent_enc_out, dim=1).reshape(n, s, -1)  # (N, Sent, H)
+        sent_enc_out = torch.split(sent_enc_out[token_masks], torch.sum(token_masks, dim=-1).tolist())
+        sent_enc_out = torch.stack([torch.mean(item, dim=0) for item in sent_enc_out], dim=0).reshape(n, s, -1)
         doc_enc_out, *_ = self.doc_enc(inputs_embeds=sent_enc_out)
         doc_enc_out = self.dropout(doc_enc_out)
         logits = self.classifier(doc_enc_out).reshape(n * s, -1)

train.py
@@ -15,7 +15,7 @@ def train_model(device, model, dataloaders, args, logger):
     else:
         t_total = len(dataloaders["train"]) // args.gradient_accumulation_steps * args.num_epochs
 
-    optimiser = torch.optim.Adam(model.parameters())
+    optimiser = torch.optim.Adam(model.parameters(), weight_decay=5e-4)
     criterion = torch.nn.CrossEntropyLoss()
 
     # Apex
@@ -47,7 +47,6 @@ def train_model(device, model, dataloaders, args, logger):
     logger.info("  Total optimization steps = %d", t_total)
 
     curr_steps, global_step = 0, 0
-    # num_segs = args.num_segs
     tr_loss, logging_loss = 0.0, 0.0
     running_corrects, acc_total = 0, 0
 
@@ -112,7 +111,6 @@ def train_model(device, model, dataloaders, args, logger):
                     if args.local_rank in [-1, 0]:
                         tb_writer.add_scalar("Train_Acc", train_acc, global_step)
                         tb_writer.add_scalar("Train_Loss", train_loss, global_step)
-                        # tb_writer.add_scalar("Num_Segs", num_segs, global_step)
                         tb_writer.add_scalar("Val_Acc", val_acc, global_step)
                         tb_writer.add_scalar("Val_Loss", val_loss, global_step)
 